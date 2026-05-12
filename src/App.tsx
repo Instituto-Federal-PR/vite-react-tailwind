f5f95ed10162faf5ebfa4c4d614d8bd479500b81
gileduardo <gilgea@gmail.com>
2026-05-12 20:43:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+function App() {
+  
+  return (
+  
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <span className="text-5xl font-bold text-sky-400 drop-shadow-lg">
+        Tailwind + Vite/React funcionando!
+      </span>
+    </div>
+
+  )
+}
+
+export default App
